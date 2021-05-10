d4f572bbea52c753fad970b35af785e1648c4690
dewei zhang <377805163@qq.com>
2021-05-10 21:43:34 +0800

    4done

--- router.js
+++ router.js
@@ -1,36 +1,72 @@
 const express=require('express')
+const Students=require('./students')
 
 const router=express.Router()
 
-
+//学生展示页面
 router.get('/students',function(request,response){
 
+        Students.findStu(function(err,students){
+                if(err)return response.status(500).send('Server Error');
 
+                response.render('index.html',{students})
+        })
 })
 
+
+//添加学生页面
 router.get('/students/new',function(request,response){
 
         response.render('new.html')
 })
 
+
+//添加学生
 router.post('/students/new',function(request,response){
 
+        Students.addStu(request.body,function(err){
+
+                if(err)return response.status(500).send('Server Error');
+                response.redirect('/students')
+        })
 
 })
 
+
+//编辑学生页面
 router.get('/students/edit',function(request,response){
 
+        Students.findByID(request.query.id,function(err,student){
+                if(err)return response.status(500).send('Server Error');
+
+                response.render('edit.html',{student})
+        })
 
 })
 
+
+//编辑学生
 router.post('/students/edit',function(request,response){
 
+        Students.updateByID(request.body,function(err){
+
+                if(err)return response.status(500).send('Server Error');
+
+                response.redirect('/students')
+        })
 
 })
 
+
+//删除学生
 router.get('/students/delete',function(request,response){
 
-
+        Students.deleteByID(request.query.id, function (err) {
+                if (err) {
+                  return response.status(500).send('Server error.')
+                }
+                response.redirect('/students')
+              })
 })
 
 
--- students.js
+++ students.js
@@ -35,7 +35,8 @@ exports.addStu=function(student,callback){
         if(err)return callback(err);
 
         const students=JSON.parse(data).students
-        students.unshift(student)
+        student.id = students.length + 1
+        students.push(student)
 
         const newData=JSON.stringify({students})
 
@@ -55,6 +56,34 @@ exports.updateByID=function(student,callback){
         if(err)return callback(err);
 
         const students=JSON.parse(data).students
+
         student.id=parseInt(student.id)
+        const theStu=students.find(item=>item.id===student.id)
+        for(var key in student){
+            theStu[key]=student[key]
+        }
+
+        const newData=JSON.stringify({students})
+        fs.writeFile(db,newData,function(err){
+            if(err)return callback(err);
+            callback(null)
+        })
+    })
+}
+
+exports.deleteByID=function(id,callback){
+    fs.readFile(db,'utf8',function(err,data){
+
+        if(err)return callback(err);
+        const students=JSON.parse(data).students
+
+        const deleteStu=students.findIndex(item=>item.id===parseInt(id))
+        students.splice(deleteStu,1)
+
+        const newData=JSON.stringify({students})
+        fs.writeFile(db,newData,function(err){
+            if(err)return callback(err);
+            callback(null)
+        })
     })
 }
